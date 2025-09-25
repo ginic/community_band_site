8044426ec80ed0baf8f7c98bbca4bfc7d65905d8
Virginia Partridge <vcpartridge@gmail.com>
2025-09-25 18:13:55 -0400

    Responsive image handling with eleventy-img

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,6 +1,27 @@
+const Image = require("@11ty/eleventy-img");
+
 module.exports = (config) => {
     config.addPassthroughCopy("src/_images");
     config.addPassthroughCopy("src/_css");
+
+    // Add responsive image shortcode
+    config.addAsyncShortcode("image", async function(src, alt, sizes = "100vw") {
+        const metadata = await Image(src, {
+            widths: [320, 640, 960, 1200, 1800, null],
+            formats: ["jpeg", "png"],
+            outputDir: "./_site/img/",
+            urlPath: "/img/",
+        });
+
+        const imageAttributes = {
+            alt,
+            sizes,
+            loading: "lazy",
+            decoding: "async",
+        };
+
+        return Image.generateHTML(metadata, imageAttributes);
+    });
 
     return {
         markdownTemplateEngine: 'njk',
